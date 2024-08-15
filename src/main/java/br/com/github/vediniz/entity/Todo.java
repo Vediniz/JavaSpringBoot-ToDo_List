5e958a2a197ba33cbc35e7622f35dca91342182f
Vediniz <veruskalima138@gmail.com>
2024-08-15 19:15:23 -0300

    feat: adding entity
    Signed-off-by: Vediniz <veruskalima138@gmail.com>

---
+++ src/main/java/br/com/github/vediniz/entity/Todo.java
@@ -0,0 +1,69 @@
+package br.com.github.vediniz.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    private boolean status;
+    private int priority;
+
+    public Todo() {}
+
+    public Todo(String name, String description, int priority, boolean status) {
+        this.name = name;
+        this.description = description;
+        this.priority = priority;
+        this.status = status;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
