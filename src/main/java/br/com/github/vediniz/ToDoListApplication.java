66ff62ccc1cff34ce565cd49a50451e50b62390b
Vediniz <veruskalima138@gmail.com>
2024-08-15 19:15:52 -0300

    feat: adding aplication
    Signed-off-by: Vediniz <veruskalima138@gmail.com>

---
+++ src/main/java/br/com/github/vediniz/ToDoListApplication.java
@@ -0,0 +1,11 @@
+package br.com.github.vediniz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListApplication.class, args);
+    }
+}
